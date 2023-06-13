b8cc3910c097929d1e872159f8d0cb1ca2c1543c
zhufucdev <1095291434@qq.com>
2023-06-13 21:33:17 +0800

    Lift canvas size out of hard code

--- local_display.py
+++ local_display.py
@@ -4,8 +4,8 @@ from display import Display
 
 
 class LocalDisplay(Display):
-    def __init__(self):
-        super().__init__((800, 480))
+    def __init__(self, width: int, height: int):
+        super().__init__((width, height))
 
     def draw(self, canvas: Image):
         canvas.show('display')
