c6ee5ef2a3b9061cccbda165d7da9cac6999fad0
zhufucdev <1095291434@qq.com>
2023-10-04 13:11:21 +0800

    Adopt newer version of epui

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import threading
 from epui.ui import *
 from epui.schedule import GoogleCalendarProvider, CalendarView, SquareDateView, CalendarProvider, Event
 from epui.weather import LargeWeatherView, WeatherTrendView, CaiYunWeatherProvider, Location, WeatherEffectiveness, \
-    CaiYunAPIProvider
+    CaiYunAPIProvider, WeatherFlowView, WeatherProvider, Weather, HeFengWeatherProvider, HeFengAPIProvider
 from PIL import Image, ImageDraw
 
 
@@ -36,6 +36,16 @@ class CachedCalendarProvider(CalendarProvider):
         return self.__cache
 
 
+class SlicedWeatherProvider(WeatherProvider):
+    def __init__(self, parent: WeatherProvider, ranging: range):
+        super().__init__(location=parent.get_location(), temperature_unit=parent.get_temperature_unit())
+        self.__parent = parent
+        self.__range = ranging
+
+    def get_weather(self) -> List[Weather]:
+        return self.__parent.get_weather()[self.__range.start:self.__range.stop]
+
+
 def start_schedule(cached_provider: CachedCalendarProvider):
     while True:
         try:
@@ -52,20 +62,21 @@ def construct_ui(draw, canvas_size: Tuple[int, int]) -> Context:
     resources.resources_dir.append('proper_res')
 
     context = Context(draw, canvas_size)
-    vgroup = VGroup(context,
-                    alignment=ViewAlignmentHorizontal.CENTER,
-                    prefer=ViewMeasurement.default(
-                        width=ViewSize.MATCH_PARENT,
-                        height=ViewSize.MATCH_PARENT
-                    ))
+    hgroup_root = HGroup(context, prefer=ViewMeasurement.default(size=ViewSize.MATCH_PARENT))
+    vgroup_left = VGroup(context,
+                         alignment=ViewAlignmentHorizontal.CENTER)
+    vgroup_right = VGroup(context,
+                          alignment=ViewAlignmentHorizontal.CENTER,
+                          prefer=ViewMeasurement.default(size=ViewSize.MATCH_PARENT))
     header = HGroup(context,
                     prefer=ViewMeasurement.default(width=ViewSize.MATCH_PARENT),
                     alignment=ViewAlignmentVertical.CENTER)
-    vgroup.add_view(header)
-    context.root_group.add_view(vgroup)
+    vgroup_left.add_view(header)
+    hgroup_root.add_views(vgroup_left, vgroup_right)
+    context.root_group.add_view(hgroup_root)
 
-    with open(resources.get_file('caiyun_key'), 'rb') as f:
-        weather_api_provider = CaiYunAPIProvider(
+    with open(resources.get_file('hefeng_key'), 'rb') as f:
+        weather_api_provider = HeFengAPIProvider(
             api_key=f.read().decode('utf-8'),
             location=Location(
                 latitude=32.20635,
@@ -73,16 +84,18 @@ def construct_ui(draw, canvas_size: Tuple[int, int]) -> Context:
             )
         )
 
-    weather_provider = CaiYunWeatherProvider(
+    weather_provider = HeFengWeatherProvider(
         weather_api_provider
     )
+    weather_provider_front = SlicedWeatherProvider(weather_provider, range(0, 12))
+    weather_provider_rear = SlicedWeatherProvider(weather_provider, range(12, -1))
     with open(resources.get_file('gcp_key'), 'rb') as f:
         cid, key = f.read().decode('utf-8').splitlines()
         calendar_provider = CachedCalendarProvider(GoogleCalendarProvider(
             name='GCP',
             calendar_id=cid,
             api_key=key,
-            max_results=4,
+            max_results=8,
         ))
 
     calendar_view = CalendarView(
@@ -92,7 +105,6 @@ def construct_ui(draw, canvas_size: Tuple[int, int]) -> Context:
         prefer=ViewMeasurement.default(
             size=ViewSize.MATCH_PARENT,
             margin_top=20,
-            margin_bottom=20,
         )
     )
 
@@ -102,17 +114,22 @@ def construct_ui(draw, canvas_size: Tuple[int, int]) -> Context:
         prefer=ViewMeasurement.default(margin=20)
     )
 
-    weather_trend_view = WeatherTrendView(
-        context,
-        title='24 hours',
-        provider=weather_provider,
-        effect=WeatherEffectiveness.HOURLY,
-        value=lambda w: w.temperature,
-        line_width=3,
-        prefer=ViewMeasurement.default(
-            size=ViewSize.MATCH_PARENT,
+    def default_weather_flow(provider: WeatherProvider):
+        return WeatherFlowView(
+            context,
+            provider=provider,
+            effect=WeatherEffectiveness.HOURLY,
+            prefer=ViewMeasurement.default(
+                width=ViewSize.MATCH_PARENT,
+            )
         )
+
+    weather_grid = VGroup(context, prefer=ViewMeasurement.default(width=ViewSize.MATCH_PARENT))
+    weather_grid.add_views(
+        default_weather_flow(weather_provider_front),
+        default_weather_flow(weather_provider_rear)
     )
+    vgroup_right.add_view(weather_grid)
 
     header.add_views(
         SquareDateView(
@@ -121,11 +138,10 @@ def construct_ui(draw, canvas_size: Tuple[int, int]) -> Context:
                 height=ViewSize.MATCH_PARENT,
                 width=180
             ),
-            current_week_offset=-35),
+            current_week_offset=-34),
         large_weather_view,
-        calendar_view
     )
-    vgroup.add_view(weather_trend_view)
+    vgroup_left.add_view(calendar_view)
 
     def refresh_calendar():
         if calendar_provider.invalidate():
@@ -133,7 +149,8 @@ def construct_ui(draw, canvas_size: Tuple[int, int]) -> Context:
 
     def refresh_weather():
         weather_provider.invalidate()
-        weather_trend_view.refresh()
+        for view in weather_grid.get_children():
+            view.refresh()
         large_weather_view.refresh()
 
     schedule.every(3).minutes.do(refresh_calendar)
@@ -151,14 +168,19 @@ def main(display: Display, context: Context, img: Image.Image):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true', help='Debug mode')
+    parser.add_argument('-b', '--bounds', action='store_true', help='Show bounds')
     args = parser.parse_args()
+
     if args.debug:
         from local_display import LocalDisplay
+
         display = LocalDisplay(800, 480)
     else:
         from gdey_display import GdeyDisplay
+
         display = GdeyDisplay()
 
+    View.draw_bounds_box = args.bounds
 
     img = Image.new('L', display.canvas_size, 255)
     draw = ImageDraw.Draw(img)
